ae69ca6274006cd44ba78457a14f5ab0f1a83980
skranz-auto <xsx50@uni-ulm.de>
2023-12-19 08:50:25 +0100

    Globally replace project.dir with project_dir

--- R/ejd_gha.R
+++ R/ejd_gha.R
@@ -24,29 +24,29 @@ update_gha_repbox_art = function(art, repodir,db) {
     return()
   }
   ejd_art = art_to_ejd_art(art,db)
-  project.dir = paste0(repodir,"/project")
-  if (!dir.exists(file.path(project.dir,"meta"))) {
-    dir.create(file.path(project.dir,"meta"))
+  project_dir = paste0(repodir,"/project")
+  if (!dir.exists(file.path(project_dir,"meta"))) {
+    dir.create(file.path(project_dir,"meta"))
   }
-  saveRDS(ejd_art, file.path(project.dir, "meta","ejd_art.Rds"))
+  saveRDS(ejd_art, file.path(project_dir, "meta","ejd_art.Rds"))
 
   oi_id = str.between(art$data_url,"openicpsr/project/","/")
   yaml::write_yaml(list(repo_type="oi",repo_id = oi_id), file.path(repodir, "repbox_config.yml"))
 
 }
 
-write_ejd_gha_status = function(project.dir, status, runid, log.txt) {
+write_ejd_gha_status = function(project_dir, status, runid, log.txt) {
   runid = as.character(runid)
 
   # Write current state
-  gha.dir = file.path(project.dir, "gha")
+  gha.dir = file.path(project_dir, "gha")
   if (!dir.exists(gha.dir)) dir.create(gha.dir, recursive = TRUE)
   writeLines(status, file.path(gha.dir,"gha_status.txt"))
   writeLines(runid, file.path(gha.dir,"gha_runid.txt"))
   writeLines(log.txt, file.path(gha.dir,"gha_log.txt"))
 
   # Write permanent state
-  #gha.dir = file.path(project.dir, "gha", format(Sys.time(),"%Y_%m_%d-%H%M"))
+  #gha.dir = file.path(project_dir, "gha", format(Sys.time(),"%Y_%m_%d-%H%M"))
   #if (!dir.exists(gha.dir)) dir.create(gha.dir, recursive = TRUE)
   #writeLines(status, file.path(gha.dir,"gha_status.txt"))
   #writeLines(runid, file.path(gha.dir,"gha_runid.txt"))
